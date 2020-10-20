14f369f7e1ef186937998f7d645b607a46c34eb6
IrakliGiorgadze <ika.giorgadze@gmail.com>
2020-10-20 22:24:13 +0400

    basic setup, add components and forms

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe("A Test Recipe", "Test Description", "https://images.squarespace-cdn.com/content/v1/5d96a22649af0e131e78ca1c/1571925942764-DJRYCQKB3B2OC3RM951L/ke17ZwdGBToddI8pDm48kNvT88LknE-K9M4pGNO0Iqd7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z5QPOohDIaIeljMHgDF5CVlOqpeNLcJ80NK65_fV7S1USOFn4xF8vTWDNAUBm5ducQhX-V3oVjSmr829Rco4W2Uo49ZdOtO_QXox0_W7i2zEA/ThumbervBlog.jpg?format=1000w"),
+    new Recipe("A Test Recipe", "Test Description", "https://images.squarespace-cdn.com/content/v1/5d96a22649af0e131e78ca1c/1571925942764-DJRYCQKB3B2OC3RM951L/ke17ZwdGBToddI8pDm48kNvT88LknE-K9M4pGNO0Iqd7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z5QPOohDIaIeljMHgDF5CVlOqpeNLcJ80NK65_fV7S1USOFn4xF8vTWDNAUBm5ducQhX-V3oVjSmr829Rco4W2Uo49ZdOtO_QXox0_W7i2zEA/ThumbervBlog.jpg?format=1000w"),
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
