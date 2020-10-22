ecb022cc3042fb7b3aca74b5b3bb836f1b337371
IrakliGiorgadze <ika.giorgadze@gmail.com>
2020-10-23 03:04:39 +0400

    add components & databinding

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 import { Recipe } from '../recipe.model';
 
@@ -8,14 +8,20 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
   recipes: Recipe[] = [
-    new Recipe("A Test Recipe", "Test Description", "https://images.squarespace-cdn.com/content/v1/5d96a22649af0e131e78ca1c/1571925942764-DJRYCQKB3B2OC3RM951L/ke17ZwdGBToddI8pDm48kNvT88LknE-K9M4pGNO0Iqd7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z5QPOohDIaIeljMHgDF5CVlOqpeNLcJ80NK65_fV7S1USOFn4xF8vTWDNAUBm5ducQhX-V3oVjSmr829Rco4W2Uo49ZdOtO_QXox0_W7i2zEA/ThumbervBlog.jpg?format=1000w"),
-    new Recipe("A Test Recipe", "Test Description", "https://images.squarespace-cdn.com/content/v1/5d96a22649af0e131e78ca1c/1571925942764-DJRYCQKB3B2OC3RM951L/ke17ZwdGBToddI8pDm48kNvT88LknE-K9M4pGNO0Iqd7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z5QPOohDIaIeljMHgDF5CVlOqpeNLcJ80NK65_fV7S1USOFn4xF8vTWDNAUBm5ducQhX-V3oVjSmr829Rco4W2Uo49ZdOtO_QXox0_W7i2zEA/ThumbervBlog.jpg?format=1000w"),
+    new Recipe("A Test Recipe", "Test Description", "https://i.pinimg.com/564x/44/c1/11/44c111473339e98604cf5c3b0c37d5f0.jpg"),
+    new Recipe("Another Test Recipe", "Test Description", "https://i.pinimg.com/564x/44/c1/11/44c111473339e98604cf5c3b0c37d5f0.jpg"),
   ];
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
