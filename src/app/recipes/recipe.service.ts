55c4562478e200e19d8e31d0d7e70173bc0bee04
IrakliGiorgadze <ika.giorgadze@gmail.com>
2020-11-12 18:02:14 +0400

    add firebase

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -10,18 +10,25 @@ import { Recipe } from './recipe.model';
 export class RecipeService {
     recipesChanged = new Subject<Recipe[]>();
 
-    private recipes: Recipe[] = [
-        new Recipe("A Test Recipe", "Test Description", "https://i.pinimg.com/564x/44/c1/11/44c111473339e98604cf5c3b0c37d5f0.jpg", [
-            new Ingredient("Meat", 1),
-            new Ingredient("French Fries", 9),
-        ]),
-        new Recipe("Another Test Recipe", "Test Description", "https://i.pinimg.com/564x/44/c1/11/44c111473339e98604cf5c3b0c37d5f0.jpg", [
-            new Ingredient("Meat", 1),
-            new Ingredient("French Fries", 9),
-        ]),
-    ];
+    // private recipes: Recipe[] = [
+    //     new Recipe("A Test Recipe", "Test Description", "https://i.pinimg.com/564x/44/c1/11/44c111473339e98604cf5c3b0c37d5f0.jpg", [
+    //         new Ingredient("Meat", 1),
+    //         new Ingredient("French Fries", 9),
+    //     ]),
+    //     new Recipe("Another Test Recipe", "Test Description", "https://i.pinimg.com/564x/44/c1/11/44c111473339e98604cf5c3b0c37d5f0.jpg", [
+    //         new Ingredient("Meat", 1),
+    //         new Ingredient("French Fries", 9),
+    //     ]),
+    // ];
+
+    private recipes: Recipe[] = [];
 
     constructor(private slService: ShoppingListService) { }
+
+    setRecipes(recipes: Recipe[]) {
+        this.recipes = recipes;
+        this.recipesChanged.next(this.recipes.slice());
+    }
 
     getRecipes() {
         return this.recipes.slice();
